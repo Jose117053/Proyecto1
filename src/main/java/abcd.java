1cfb9c6d124d0adb9dca65936e84f304e561cee9
Jose117053 <adolfo19420@gmail.com>
2023-10-17 23:34:43 -0600

    Ya no me pide logearme a cada rato

--- src/main/java/abcd.java
+++ src/main/java/abcd.java
@@ -2,5 +2,6 @@ public class abcd {
     public static void main(String[] args){
         System.out.println("main");
         System.out.println("usuario de git");
+        System.out.println("Ya no me pide logearme a cada rato");
     }
 }
